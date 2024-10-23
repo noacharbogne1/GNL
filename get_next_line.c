7b79bcec9194a9e8e16c7a0373cbc794581c604a
Noa Charbogne <noa.charbogne@orange.fr>
2024-10-23 11:33:29 +0200

    midi mercredi

--- get_next_line.c
+++ get_next_line.c
@@ -6,35 +6,67 @@
 /*   By: ncharbog <ncharbog@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 10:59:51 by ncharbog          #+#    #+#             */
-/*   Updated: 2024/10/22 16:31:16 by ncharbog         ###   ########.fr       */
+/*   Updated: 2024/10/23 11:31:21 by ncharbog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_trim_line(static char *stock)
+{
+	char	*dest;
+	int		len;
+	int		end;
+	int		i;
+
+	i = 0;
+	while(stock[i] != '\n')
+		i++;
+	len = i;
+	i = 0;
+	dest = malloc ((len + 1) * sizeof(char));
+	if (!dest)
+		return (0);
+	while (i < len)
+	{
+		dest[i] = stock[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest); // attention il manque un free !
+}
+
 char	*get_next_line(int fd)
 {
 	char			*buf;
-	char			*next_line;
+	static char		*stock;
 	unsigned int	count;
+	unsigned int	check;
 	unsigned int	i;
 
+	check = 0;
 	i = 0;
+	count = 1;
 	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buf)
 		return (0);
-	while (buf[i] != '\n')
+	stock = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!stock)
+		return (0);
+	while (stock[i])
+	{
+		stock[i] = '\0';
+		i++;
+	}
+	while (check == 0 && count > 0)
 	{
 		count = read(fd, buf, BUFFER_SIZE);
-		while (i < count)
-		{
-			if (buf[i] == '\n')
-				return (ft_strcpy(buf));
-			i++;
-		}
-		i = 0;
-		strjoin(static, buf);
+		buf[BUFFER_SIZE + 1] = '\0';
+		stock = ft_strjoin(buf, stock);
+		check = ft_check_n(stock);
+		if (check == 0)
+			ft_stock_free(stock);
 	}
 	free(buf);
-	return (next_line);
+	return (ft_trim_line(stock));
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,19 +6,29 @@
 /*   By: ncharbog <ncharbog@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 11:00:02 by ncharbog          #+#    #+#             */
-/*   Updated: 2024/10/22 15:58:06 by ncharbog         ###   ########.fr       */
+/*   Updated: 2024/10/23 11:07:45 by ncharbog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_strcpy(char *s)
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_copy(char *s)
 {
 	char	*dest;
 	int		i;
 
 	i = 0;
-	if (!s || fd < 0)
+	if (!s)
 		return ;
 	while (s[i])
 	{
@@ -26,4 +36,36 @@ char	*ft_strcpy(char *s)
 		i++;
 	}
 	return (dest)
+}
+
+int	ft_check_n(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strjoin(char const *buf, static char *stock)
+{
+	int		i;
+	int		len;
+
+	i = 0;
+	if (!buf)
+		return (0);
+	len = ft_strlen(stock);
+	while (buf[i])
+	{
+		stock[len] = buf[i];
+		len++;
+		i++;
+	}
+	return (stock);
 }
