30aeb29ec5f656f85cfd4b4c1eb0beeab95f2179
Noa Charbogne <noa.charbogne@orange.fr>
2024-10-24 10:54:46 +0200

    main, brouillon, fonction mieux pensee avec le tmp

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,15 @@
 /*   By: ncharbog <ncharbog@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 10:59:51 by ncharbog          #+#    #+#             */
-/*   Updated: 2024/10/24 10:01:58 by ncharbog         ###   ########.fr       */
+/*   Updated: 2024/10/24 10:46:23 by ncharbog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_trim_line(char *str)
+char	*ft_trim_line(char *str, char *dest)
 {
-	char	*dest;
 	int		len;
-	int		end;
 	int		i;
 
 	i = 0;
@@ -24,9 +22,6 @@ char	*ft_trim_line(char *str)
 		i++;
 	len = i;
 	i = 0;
-	dest = malloc ((len + 1) * sizeof(char));
-	if (!dest)
-		return (0);
 	while (i < len)
 	{
 		dest[i] = str[i];
@@ -64,43 +59,42 @@ char	*ft_rest_end(char *str)
 char	*get_next_line(int fd)
 {
 	char			*buf;
-	char			*next_line;
+	char			*tmp;
 	static char		*stock;
 	unsigned int	count;
 	unsigned int	check;
 
-	check = 0;
+	check = 1;
 	count = 0;
 	*stock = "";
 	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buf)
 		return (0);
-	while (check == -1 && count > 0)
+	while (check)
 	{
 		count = read(fd, buf, BUFFER_SIZE);
 		buf[BUFFER_SIZE + 1] = '\0';
 		check = ft_check_n(buf);
 		if (check >= 0)
 		{
-			next_line = malloc((check + 1) * sizeof(char));
-			if (!next_line)
+			stock = malloc((ft_strlen(stock) + check + 1) * sizeof(char));
+			if (!stock)
 				return (0);
-			next_line = ft_trim_line(next_line, buf);
+			stock = ft_trim_line(buf, stock);
 			ft_rest_end(buf);
 		}
-		while (check == -1)
+		if (check == -1)
 		{
+			tmp = stock;
 			stock = malloc((ft_strlen(stock) + count + 1) * sizeof(char));
 			if (!stock)
 				return (0);
-			// free(stock);
-			stock = ft_strjoin(stock, buf);
-			count = read(fd, buf, BUFFER_SIZE);
-			buf[BUFFER_SIZE + 1] = '\0';
-			check = ft_check_n(buf);
+			stock = ft_strjoin(tmp, buf);
+			free(tmp);
+			tmp = NULL;
 		}
-
+		break;
 	}
 	free(buf);
-	return (next_line);
+	return (stock);
 }
--- main.c
+++ main.c
@@ -1,26 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ncharbog <ncharbog@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/22 11:00:20 by ncharbog          #+#    #+#             */
-/*   Updated: 2024/10/24 10:51:22 by ncharbog         ###   ########.fr       */
+/*   Created: 2024/10/24 10:24:01 by ncharbog          #+#    #+#             */
+/*   Updated: 2024/10/24 10:25:57 by ncharbog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#include <stdio.h>
 
-# include <unistd.h>
-# include <stdlib.h>
+int	main()
+{
+	int fd;
 
-char	*get_next_line(int fd);
-char	*ft_rest_end(char *str);
-char	*ft_trim_line(char *str, char *dest);
-int		ft_strlen(char *s);
-int		ft_check_n(char *s);
-char	*ft_strjoin(char *s1, char *s2);
-
-#endif
+	fd = open("test", O_RDONLY);
+	printf("%s", get_next_line(fd));
+}
